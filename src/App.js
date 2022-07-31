fb9feea5ede1ae0767bc578853aaba1dbed52afb
yesiman <fab.grenouillet@gmail.com>
2022-07-31 21:41:03 +0400

    ajout composants et utilisation du hook pour maj title selon changement page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,65 @@
-import logo from './logo.svg';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
+import Home from './components/Home';
+import Contact from './components/Contact';
 
-function App() {
+import LangSelector from './components/LangSelector';
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <div className="row rowmenu">
+          <div className="col-10">
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Accueil</Link>
+                </li>
+                <li>
+                  <Link to="/contact">Contact</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div className="col-2">
+            <LangSelector/>
+          </div>
+        </div>
+        <Routes>
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/"  element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+
+
+
+
+/*
+function About() {
   return (
     <div className="App">
+      <div className="main-header">
+        <div className="row">
+          <div className="mainmenu col-10">
+              <a href="index.html">Home</a>
+              <a href="contact.html">Contact</a>                     
+          </div>
+          <div className="rightmenu col-2">
+              <LangSelector/>
+          </div>
+        </div>
+      </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -23,3 +79,4 @@ function App() {
 }
 
 export default App;
+*/
